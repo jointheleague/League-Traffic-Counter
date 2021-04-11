d41f3ef6c1e1c7a19da2130984f54ab655fd97f6
Samuel Sharp <samuelsharp4@gmail.com>
2021-04-11 12:49:29 -0700

    Add login screen handler

--- app/src/main/java/org/jointheleague/traffic/MainActivity.java
+++ app/src/main/java/org/jointheleague/traffic/MainActivity.java
@@ -1,6 +1,16 @@
 package org.jointheleague.traffic;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.*;
+import com.android.volley.toolbox.*;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -8,5 +18,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button startButton = (Button) findViewById(R.id.button);
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText trafficID = (EditText) findViewById(R.id.LTIDinput);
+                RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+                StringRequest stringRequest = new StringRequest(Request.Method.GET, "https://leaguetraffic-auth.samuelblue.repl.co/platform/authCheck/" + trafficID.getText().toString(),
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                                Context context = getApplicationContext();
+                                CharSequence text = response;
+                                Toast toast = Toast.makeText(context, text, Toast.LENGTH_SHORT);
+                                toast.show();
+                            }
+                        }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Context context = getApplicationContext();
+                        CharSequence text = error.toString();
+                        Toast toast = Toast.makeText(context, text, Toast.LENGTH_SHORT);
+                        toast.show();
+                    }
+                });
+                queue.add(stringRequest);
+            }
+        });
     }
 }
